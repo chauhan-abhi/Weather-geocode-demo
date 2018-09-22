6f15f50836b72e90838ce8c325e174bdf33eecca
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-09-23 00:05:54 +0530

    request geocode using Promises

---
+++ playground/promise-request.js
@@ -0,0 +1,32 @@
+
+const request = require('request')
+
+var geocodeAddress = (address) => {
+    return new Promise((resolve, reject) => {
+    var encodedAddress = encodeURIComponent(address)
+    request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBKDshOx98TyQeDC7C2Wj2UzlApnbLgynw&address=${encodedAddress}`,
+    json: true
+    }, (error, response, body) => {
+    // callback after fetching result process result
+    //console.log(JSON.stringify(response, undefined, 2))
+    if(error) {
+        reject('Unable to connect to google servers')
+    } else if(body.status === 'ZERO_RESULTS') {
+        reject('Unable to find that address')
+    } else if(body.status === 'OK') {
+        resolve({
+            address: body.results[0].formatted_address,
+            latitude: body.results[0].geometry.location.lat,
+            longitude: body.results[0].geometry.location.lng
+        })
+    }
+})
+    })
+}
+
+geocodeAddress('201301').then((location) => {
+    console.log(JSON.stringify(location, undefined, 2))
+}, (errorMessage) => {
+    console.log(errorMessage)
+})
